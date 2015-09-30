2cfabc2d7c66fb7b79440273ad68877f61666414
rich <rich@10.161.54.130>
2015-09-30 10:02:26 -0700

    update AD login

--- js/db_access.js
+++ js/db_access.js
@@ -122,7 +122,6 @@ function db_insertManager(MgrName, MgrEmail, MgrTitle, MgrPhone, Department) {
     return ResultID;
 }
 
-// update
 function db_insertKeyfob(StatusID, UserID, ManagerID, Justification, Replace) {
     var ResultID = "";
     $.ajax({
