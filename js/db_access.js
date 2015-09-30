3dd6bad70fd462803f5607dea85caf8223b2d725
rich <rich@10.161.54.130>
2015-09-30 15:20:45 -0700

    update AD login

--- js/db_access.js
+++ js/db_access.js
@@ -122,12 +122,12 @@ function db_insertManager(MgrName, MgrEmail, MgrTitle, MgrPhone, Department) {
     return ResultID;
 }
 
-function db_insertKeyfob(StatusID, UserID, ManagerID, Justification, Replace) {
+function db_insertKeyfob(StatusID, UserID, ManagerID, CreateDate, Justification, Replace) {
     var ResultID = "";
     $.ajax({
         type:"POST",
         url:"php/db_insertKeyfob.php",
-        data:{StatusID:StatusID, UserID:UserID, ManagerID:ManagerID, Justification:Justification, Replace:Replace},
+        data:{StatusID:StatusID, UserID:UserID, ManagerID:ManagerID, CreateDate:CreateDate, Justification:Justification, Replace:Replace},
         async: false,  
         success:function(data) {
             ResultID = JSON.parse(data);
